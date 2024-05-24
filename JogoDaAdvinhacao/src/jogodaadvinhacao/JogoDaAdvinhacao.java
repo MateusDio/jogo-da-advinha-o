443999b6a59b13275e8f0a8cad9cbf15b1d0a7c3
MateusDio <diolondamateus@gmail.com>
2024-05-24 16:54:44 -0300

    Primeiro commit jogo

---
+++ JogoDaAdvinhacao/src/jogodaadvinhacao/JogoDaAdvinhacao.java
@@ -0,0 +1,20 @@
+
+package jogodaadvinhacao;
+
+import java.util.Random;
+
+
+public class JogoDaAdvinhacao {
+
+    
+    public int ona() {
+    Random abc = new Random();
+    return abc.nextInt(10); 
+    
+}
+
+    public static void main(String[] args) {
+      
+    }
+    
+}
